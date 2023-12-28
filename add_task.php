529d913f62e57ea2693436b132ab6a097ee625f4
Felipe Sadrak dos Santos <141746637+Felip3Sadrak@users.noreply.github.com>
2023-12-28 16:37:53 -0300

    :lipstick: Criação do Layout Inicial
    Estilização da aplicação

--- add_task.php
+++ add_task.php
@@ -24,14 +24,70 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Adicionar Tarefa</title>
+    <style>
+        body {
+            font-family: 'Arial', sans-serif;
+            margin: 0;
+            padding: 0;
+            background-color: #f8f9fa;
+        }
+
+        h2 {
+            color: #007bff;
+            text-align: center;
+            margin-top: 20px;
+        }
+
+        form {
+            max-width: 400px;
+            margin: 20px auto;
+            padding: 20px;
+            background-color: #fff;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            border-radius: 5px;
+        }
+
+        label {
+            display: block;
+            margin-bottom: 8px;
+            color: #333;
+        }
+
+        input, textarea {
+            width: 100%;
+            padding: 10px;
+            margin-bottom: 15px;
+            border: 1px solid #ddd;
+            border-radius: 4px;
+            box-sizing: border-box;
+        }
+
+        input[type="submit"] {
+            background-color: #007bff;
+            color: #fff;
+            cursor: pointer;
+        }
+
+        input[type="submit"]:hover {
+            background-color: #0056b3;
+        }
+    </style>
 </head>
 <body>
     <h2>Adicionar Tarefa</h2>
     <form method="post" action="add_task.php">
-        Nome da Tarefa: <input type="text" name="name" required><br>
-        Descrição: <textarea name="description"></textarea><br>
-        Data de Vencimento: <input type="date" name="due_date" required><br>
-        Responsável: <input type="text" name="assigned_to" required><br>
+        <label for="name">Nome da Tarefa:</label>
+        <input type="text" name="name" id="name" required>
+
+        <label for="description">Descrição:</label>
+        <textarea name="description" id="description"></textarea>
+
+        <label for="due_date">Data de Vencimento:</label>
+        <input type="date" name="due_date" id="due_date" required>
+
+        <label for="assigned_to">Responsável:</label>
+        <input type="text" name="assigned_to" id="assigned_to" required>
+
         <input type="submit" value="Adicionar Tarefa">
     </form>
 </body>
--- index.php
+++ index.php
@@ -10,30 +10,84 @@ $result = $conn->query($sql);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Gerenciador de Projetos</title>
+
+    <title>Gerenciador de Tarefas</title>
+    <style>
+        body {
+            font-family: 'Arial', sans-serif;
+            margin: 0;
+            padding: 0;
+            background-color: #f8f9fa;
+        }
+
+        h2 {
+            color: #007bff;
+            text-align: center;
+            margin-top: 20px;
+        }
+
+        a {
+            display: block;
+            width: 150px;
+            margin: 20px auto;
+            padding: 10px;
+            background-color: #007bff;
+            color: #fff;
+            text-align: center;
+            text-decoration: none;
+            border-radius: 5px;
+        }
+
+        table {
+            width: 80%;
+            margin: 20px auto;
+            border-collapse: collapse;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            background-color: #fff;
+        }
+
+        th, td {
+            padding: 15px;
+            text-align: left;
+            border-bottom: 1px solid #ddd;
+        }
+
+        th {
+            background-color: #007bff;
+            color: #fff;
+        }
+
+        tr:hover {
+            background-color: #f5f5f5;
+        }
+    </style>
 </head>
 <body>
-    <h2>Tarefas</h2>
+    <h2>Gerenciador de Tarefas</h2>
     <a href="add_task.php">Adicionar Tarefa</a>
-    <table border="1">
-        <tr>
-            <th>Nome</th>
-            <th>Descrição</th>
-            <th>Data de Vencimento</th>
-            <th>Responsável</th>
-            <th>Status</th>
-        </tr>
-        <?php
-        while ($row = $result->fetch_assoc()) {
-            echo "<tr>";
-            echo "<td>{$row['name']}</td>";
-            echo "<td>{$row['description']}</td>";
-            echo "<td>{$row['due_date']}</td>";
-            echo "<td>{$row['assigned_to']}</td>";
-            echo "<td>{$row['status']}</td>";
-            echo "</tr>";
-        }
-        ?>
+    <table>
+        <thead>
+            <tr>
+                <th>Nome</th>
+                <th>Descrição</th>
+                <th>Data de Vencimento</th>
+                <th>Responsável</th>
+                <th>Status</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            while ($row = $result->fetch_assoc()) {
+                echo "<tr>";
+                echo "<td>{$row['name']}</td>";
+                echo "<td>{$row['description']}</td>";
+                echo "<td>{$row['due_date']}</td>";
+                echo "<td>{$row['assigned_to']}</td>";
+                echo "<td>{$row['status']}</td>";
+                echo "</tr>";
+            }
+            ?>
+        </tbody>
     </table>
 </body>
 </html>
